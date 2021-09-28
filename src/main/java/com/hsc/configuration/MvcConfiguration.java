50690240d5d5bc9052c90c82c13caea934965459
Darkcat77 <hocraft@naver.com>
2021-09-28 11:51:59 +0900

    파일 처리 추가

--- src/main/java/com/hsc/configuration/MvcConfiguration.java
+++ src/main/java/com/hsc/configuration/MvcConfiguration.java
@@ -1,14 +1,18 @@
 package com.hsc.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.hsc.interceptor.LoggerInterceptor;
 
 @Configuration
+@PropertySource("classpath:Resource.properties")
 public class MvcConfiguration implements WebMvcConfigurer {
 
 	@Override
@@ -24,5 +28,21 @@ public class MvcConfiguration implements WebMvcConfigurer {
 		multipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024); // 파일당 업로드 크기 제한 (5MB)
 		return multipartResolver;
 	}
+	
+	@Value("${resource.path}")
+	private String resourcePath;
+	
+	@Value("${upload.path}")
+	private String uploadPath;
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler(uploadPath)
+				.addResourceLocations(resourcePath);
+	}
+	
+	
+	
+	
 
 }
